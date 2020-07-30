b68d3bd3b1bfdf9bc75bc752f3426cade46afe2d
Mark Grebe <atarimacosx@gmail.com>
2020-07-30 11:50:05 -0500

    More porting

--- atari800-MacOSX/src/pbi_proto80.h
+++ atari800-MacOSX/src/pbi_proto80.h
@@ -2,10 +2,11 @@
 #define PBI_PROTO80_H_
 
 #include "atari.h"
-void PBI_PROTO80_Initialise(int *argc, char *argv[]);
+int PBI_PROTO80_Initialise(int *argc, char *argv[]);
+void PBI_PROTO80_Exit(void);
 int PBI_PROTO80_ReadConfig(char *string, char *ptr);
 void PBI_PROTO80_WriteConfig(FILE *fp);
-int PBI_PROTO80_D1GetByte(UWORD addr);
+int PBI_PROTO80_D1GetByte(UWORD addr, int no_side_effects);
 void PBI_PROTO80_D1PutByte(UWORD addr, UBYTE byte);
 int PBI_PROTO80_D1ffPutByte(UBYTE byte);
 UBYTE PBI_PROTO80_GetPixels(int scanline, int column);
