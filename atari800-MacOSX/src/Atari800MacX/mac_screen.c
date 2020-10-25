bf9b166c56c7048f37e53584580030b5ba60e3be
Mark Grebe <atarimacosx@gmail.com>
2020-10-24 20:17:43 -0500

    Fix LED FKey to be persisent

--- atari800-MacOSX/src/Atari800MacX/mac_screen.c
+++ atari800-MacOSX/src/Atari800MacX/mac_screen.c
@@ -321,7 +321,7 @@ void Screen_DrawDiskLED(void)
 void Screen_DrawHDDiskLED(void)
 {
     if (Screen_show_hd_sector_counter) {
-        UBYTE *screen = (UBYTE *) Screen_atari + Screen_visible_x1 + SMALLFONT_WIDTH * 21
+        UBYTE *screen = (UBYTE *) Screen_atari + Screen_visible_x2 - SMALLFONT_WIDTH * 15
             + (Screen_visible_y2 - SMALLFONT_HEIGHT) * Screen_WIDTH;
         if (IMG_last_op_time > 0) {
             IMG_last_op_time--;
@@ -335,7 +335,7 @@ void Screen_Draw1200LED(void)
     if (Screen_show_1200_leds && Atari800_keyboard_leds) {
         UBYTE *screen = (UBYTE *) Screen_atari + Screen_visible_x1 + SMALLFONT_WIDTH * 10
             + (Screen_visible_y2 - SMALLFONT_HEIGHT) * Screen_WIDTH;
-        UBYTE portb = PIA_PORTB | PIA_PORTB_mask;
+        UBYTE portb = PIA_PORTB;
         if ((portb & 0x04) == 0) {
             SmallFont_DrawChar(screen, SMALLFONT_L, 0x00, 0x36);
             SmallFont_DrawChar(screen + SMALLFONT_WIDTH, 1, 0x00, 0x36);
